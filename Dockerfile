9010671a5ea1dc21b761dd6775702d8533c0cf76
winlinaung <winlinaung@immin.io>
2020-10-04 16:53:28 +0630

    fix_in_docker_file

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN npm install
 COPY . .
 
 EXPOSE 8000
-CMD ["npm","run","prod-start"]
+CMD ["npm","run","start"]
--- development/startPetrolPosApi.sh
+++ development/startPetrolPosApi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker stack rm petrol_pos_api
-sleep 5
-docker build -t winlinaung/petrol_pos_api  ../
+sleep 10
+docker build -t petrol-pos  ../
 chmod -R 777 $HOME/Desktop/Customer_Project/petrol_pos_api
 docker stack deploy -c ../docker-compose.yaml petrol_pos_api
