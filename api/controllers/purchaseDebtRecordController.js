03c07d79a83dd3c7047549138b2c9dd5e5018b0e
winlinaung <winlinaung@immin.io>
2020-10-29 10:20:04 +0630

    add_search_feature

--- api/controllers/purchaseDebtRecordController.js
+++ api/controllers/purchaseDebtRecordController.js
@@ -84,13 +84,35 @@ exports.list_purchase_debt_records_with_pagination = async (req, res) => {
 		// validate options, send 400 on error
 		const sort = options.sort || {};
 		const filter = options.filter || {};
-		const limit = 10;
+		const search = options.search || {};
+		const limit = null;
 		const page = parseInt(options.page) || 1;
 
 		Object.keys(sort).map((key) => sort[key] = parseInt(sort[key]));
-		
 		console.log(`sort[key] = ${JSON.stringify(sort)}`)
 		const aggregation = [];
+
+		if (search) {
+			let searchStr = search.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+				const regex = new RegExp(`${searchStr}*[a-zA-Z0-9!@#$%^&)(+=._\\-\\*]*`, "i");
+			  aggregation.push({
+				$match: {
+				  $or: [
+					{ companyName: { $regex: regex } },
+					{ companyPhone: { $regex: regex } },
+					{ goodType: { $regex: regex } },
+					{ quantity: { $regex: regex } },
+					{ rateFixed: { $regex: regex } },
+					{ paymentType: { $regex: regex } },
+					{ total: { $regex: regex } },
+					{ createdAt: { $regex: regex } },
+					{ updatedAt: { $regex: regex } },
+				  ],
+				},
+			  })
+		  
+		  } 
+
 		if (options.sort) aggregation.push({ $sort: sort });
 
 		const skip = (page - 1) * limit;
